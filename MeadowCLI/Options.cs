0eb765bf48cd6efa9ab36c586bc77d1d7902282f
adrianstevens <adrian.stevens@gmail.com>
2019-07-31 19:24:53 -0700

    light refactor of dfu logic into its own static class

---
+++ MeadowCLI/Options.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace MeadowCLI
+{
+    public class Options
+    {
+
+    }
+}
--- MeadowCLI/Program.cs
+++ MeadowCLI/Program.cs
@@ -7,15 +7,7 @@ namespace MeadowCLI
 {
     class Program
     {
-        static string os = "nuttx.bin";
-        static string user = "nuttx_user.bin";
-
-        static int os_address = 0x08000000;
-        static int user_address = 0x08040000;
-
-        static int uploadedByteCount = 0;
-        static int totalBytes = 0;
-
+        
         public class Options
         {
             [Option('d', "dfu", Required = false, HelpText = "DFU copy os and user files. Looks for files in execution direction. To override, user 'osFile' and 'userFile'.")]
@@ -38,71 +30,13 @@ namespace MeadowCLI
             {
                 if (opts.Dfu)
                 {
-                    DfuContext.Init();
-                    var devices = DfuContext.Current.GetDevices();
-
-                    if (devices.Count < 1)
-                    {
-                        Console.WriteLine("Attach a device in DFU mode, mofo.");
-                    }
-                    else
-                    {
-                        if (!string.IsNullOrEmpty(opts.DfuOsPath))
-                        {
-                            if (!File.Exists(opts.DfuOsPath))
-                            {
-                                Console.WriteLine($"Cannot find {opts.DfuOsPath} file.");
-                                return;
-                            }
-                        }
-                        else if (!File.Exists($"{Environment.CurrentDirectory}\\{os}"))
-                        {
-                            Console.WriteLine($"Cannot find {os} file.");
-                            return;
-                        }
-
-                        if (!string.IsNullOrEmpty(opts.DfuUserPath) )
-                        {
-                            if (!File.Exists(opts.DfuUserPath))
-                            {
-                                Console.WriteLine($"Cannot find {opts.DfuUserPath} file.");
-                                return;
-                            }
-                        }
-                        else if (!File.Exists($"{Environment.CurrentDirectory}\\{user}"))
-                        {
-                            Console.WriteLine($"Cannot find {user} file.");
-                            return;
-                        }
-
-                        devices[0].Uploading += Program_Uploading;
-
-                        Upload(devices[0], $"{opts.DfuOsPath ?? Environment.CurrentDirectory + "\\" + os}", os_address);
-                        Upload(devices[0], $"{opts.DfuUserPath ?? Environment.CurrentDirectory + "\\" + user}", user_address);
-                    }
+                    DfuUpload.FlashNuttx(opts.DfuOsPath, opts.DfuUserPath);
                 }
             });
 
             Console.ReadKey();
         }
 
-        private static void Upload(DfuDevice device, string path, int address)
-        {
-            FileInfo fi = new FileInfo(path);
-            byte[] bytes = File.ReadAllBytes(path);
-            totalBytes = bytes.Length;
-
-            Console.WriteLine($"Uploading {fi.Name}");
-            uploadedByteCount = 0;
-            device.Upload(bytes, (int)address);
-            Console.WriteLine("\rdone                    ");
-        }
-
-        private static void Program_Uploading(object sender, DfuSharp.UploadingEventArgs e)
-        {
-            uploadedByteCount += e.BytesUploaded;
-
-            Console.Write($"\r{(uploadedByteCount * 100 / totalBytes)}%");
-        }
+        
     }
 }
