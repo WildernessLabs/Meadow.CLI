ef6ac7e406f2070d598e780a5738ad78090e75b2
Stuart Johnson <stuart@factoid.org>
2020-04-03 19:07:21 +0100

    Linux Udev support

---
+++ Meadow.CLI.Core/Internals/Udev/LibudevNative.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Meadow.CLI.Internals.Udev
+{
+    abstract class LibudevNative
+    {
+        static LibudevNative()
+        {
+            foreach (var instance in new LibudevNative[] { new LibudevNative1(), new LibudevNative0() })
+            {
+                try
+                {
+                    IntPtr udev = instance.udev_new();
+                    if (IntPtr.Zero != udev)
+                    {
+                        instance.udev_unref(udev);
+                        Instance = instance; break;
+                    }
+                }
+                catch
+                {
+
+                }
+            }
+        }
+
+        public static LibudevNative Instance
+        {
+            get;
+            private set;
+        }
+
+        public abstract string LibName
+        {
+            get;
+        }
+
+        public abstract IntPtr udev_new();       
+        public abstract IntPtr udev_ref(IntPtr udev);
+        public abstract void udev_unref(IntPtr udev);
+        public abstract IntPtr udev_monitor_new_from_netlink(IntPtr udev, string name);
+        public abstract void udev_monitor_unref(IntPtr monitor);
+        public abstract int udev_monitor_filter_add_match_subsystem_devtype(IntPtr monitor, string subsystem, string devtype);
+        public abstract int udev_monitor_enable_receiving(IntPtr monitor);
+        public abstract int udev_monitor_get_fd(IntPtr monitor);
+        public abstract IntPtr udev_monitor_receive_device(IntPtr monitor);
+        public abstract IntPtr udev_enumerate_new(IntPtr udev);
+        public abstract IntPtr udev_enumerate_ref(IntPtr enumerate);
+        public abstract void udev_enumerate_unref(IntPtr enumerate);
+        public abstract int udev_enumerate_add_match_subsystem(IntPtr enumerate, string subsystem);        
+        public abstract int udev_enumerate_add_match_property(IntPtr enumerate, string attr, string value);        
+        public abstract int udev_enumerate_add_match_sysattr(IntPtr enumerate, string attr, string value);
+        public abstract int udev_enumerate_scan_devices(IntPtr enumerate);
+        public abstract IntPtr udev_enumerate_get_list_entry(IntPtr enumerate);
+        public abstract IntPtr udev_list_entry_get_next(IntPtr entry);
+        public abstract string udev_list_entry_get_name(IntPtr entry);
+        public abstract IntPtr udev_device_new_from_syspath(IntPtr udev, string syspath);
+        public abstract IntPtr udev_device_ref(IntPtr device);
+        public abstract void udev_device_unref(IntPtr device);
+        public abstract string udev_device_get_devnode(IntPtr device);
+        public abstract string udev_device_get_devpath(IntPtr device);
+        public abstract string udev_device_get_syspath(IntPtr device);
+        public abstract string udev_device_get_sysname(IntPtr device);
+        public abstract string udev_device_get_devtype(IntPtr device);
+        public abstract string udev_device_get_subsystem(IntPtr device);
+        public abstract IntPtr udev_device_get_parent_with_subsystem_devtype(IntPtr device, string subsystem, string devtype);
+        public abstract string udev_device_get_sysattr_value(IntPtr device, string sysattr);
+        public abstract string udev_device_get_property_value(IntPtr device, string property);
+        public abstract int udev_device_get_is_initialized(IntPtr device);
+    }
+}
---
+++ Meadow.CLI.Core/Internals/Udev/Udev.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Meadow.CLI.Internals.Udev
+{
+    public static class Udev
+    {
+        
+        static public String[] GetUSBDevicePaths(string attribute, string value, string subSystem="tty")
+        {
+            var paths = new List<string>();
+
+            IntPtr udev = LibudevNative.Instance.udev_new();
+            if (IntPtr.Zero != udev)
+            {
+                try
+                {
+                    IntPtr enumerate = LibudevNative.Instance.udev_enumerate_new(udev);
+                    if (IntPtr.Zero != enumerate)
+                    {
+                        try
+                        {
+                            if (LibudevNative.Instance.udev_enumerate_add_match_subsystem(enumerate, subSystem) == 0 &&
+                                LibudevNative.Instance.udev_enumerate_scan_devices(enumerate) == 0)
+                            {
+                                IntPtr entry;
+                                for (entry = LibudevNative.Instance.udev_enumerate_get_list_entry(enumerate); entry != IntPtr.Zero;
+                                     entry = LibudevNative.Instance.udev_list_entry_get_next(entry))
+                                {
+                                    string syspath = LibudevNative.Instance.udev_list_entry_get_name(entry);
+                                    IntPtr device = LibudevNative.Instance.udev_device_new_from_syspath(udev, syspath);                                   
+                                    
+                                    string devnode = LibudevNative.Instance.udev_device_get_devnode(device);
+                                    
+                                    IntPtr parent = LibudevNative.Instance.udev_device_get_parent_with_subsystem_devtype(device, "usb", "usb_device");
+                                    if (IntPtr.Zero != parent)
+                                    {
+                                        string attributeValue = LibudevNative.Instance.udev_device_get_sysattr_value(parent, attribute);
+                                        if (attributeValue == value) paths.Add(devnode);
+                                    }
+                                }
+                            }
+                        }
+                        finally
+                        {
+                            LibudevNative.Instance.udev_enumerate_unref(enumerate);
+                        }
+                    }
+                }
+                finally
+                {
+                    LibudevNative.Instance.udev_unref(udev);
+                }
+            }
+
+                return paths.ToArray();
+        }
+        
+        
+    }
+}
