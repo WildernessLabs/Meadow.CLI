0e38de640594252f85f0d942c14a4fa425985f50
Stuart Johnson <stuart@factoid.org>
2020-04-14 21:12:43 +0100

    Add Udev ConnectionMonitor support

--- Meadow.CLI.Core/Internals/Udev/Udev.cs
+++ Meadow.CLI.Core/Internals/Udev/Udev.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using Meadow.CLI.DeviceMonitor;
 
 namespace Meadow.CLI.Internals.Udev
 {
@@ -118,6 +119,70 @@ namespace Meadow.CLI.Internals.Udev
         }
 
         
+        static public Connection GetDeviceFromPath(string devicePath)
+        {
+            Connection meadowDevice = new Connection();
+            if (LibudevNative.Instance == null) throw new Exception("Udev not availabe");
+        
+
+            IntPtr udev = LibudevNative.Instance.udev_new();
+            if (IntPtr.Zero != udev)
+            {
+                try
+                {
+                    IntPtr enumerate = LibudevNative.Instance.udev_enumerate_new(udev);
+                    if (IntPtr.Zero != enumerate)
+                    {
+                        try
+                        {
+                            if (LibudevNative.Instance.udev_enumerate_add_match_subsystem(enumerate, "tty") == 0 &&
+                                LibudevNative.Instance.udev_enumerate_scan_devices(enumerate) == 0)
+                            {
+                                IntPtr entry;
+                                for (entry = LibudevNative.Instance.udev_enumerate_get_list_entry(enumerate); entry != IntPtr.Zero;
+                                     entry = LibudevNative.Instance.udev_list_entry_get_next(entry))
+                                {
+                                    string syspath = LibudevNative.Instance.udev_list_entry_get_name(entry);
+                                    IntPtr device = LibudevNative.Instance.udev_device_new_from_syspath(udev, syspath);                                   
+                                    
+                                    string devnode = LibudevNative.Instance.udev_device_get_devnode(device);
+
+                                    if (devnode == devicePath)
+                                    {
+                                        IntPtr parent = LibudevNative.Instance.udev_device_get_parent_with_subsystem_devtype(device, "usb", "usb_device");
+                                        if (IntPtr.Zero != parent)
+                                        {
+                                            meadowDevice.USB = new Connection.USB_interface()
+                                            {
+                                                DevicePort = devicePath,
+                                                BusNumber = ushort.Parse(LibudevNative.Instance.udev_device_get_sysattr_value(parent, "busnum")),
+                                                DeviceNumber = ushort.Parse(LibudevNative.Instance.udev_device_get_sysattr_value(parent, "devnum")),
+                                                VendorID = Convert.ToUInt16(LibudevNative.Instance.udev_device_get_sysattr_value(parent, "idVendor"), 16),
+                                                ProductID = Convert.ToUInt16(LibudevNative.Instance.udev_device_get_sysattr_value(parent, "idProduct"), 16)
+                                            };
+                                            meadowDevice.SerialNumber = LibudevNative.Instance.udev_device_get_sysattr_value(parent, "serial");
+                        
+                                            return meadowDevice;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        finally
+                        {
+                            LibudevNative.Instance.udev_enumerate_unref(enumerate);
+                        }
+                    }
+                }
+                finally
+                {
+                    LibudevNative.Instance.udev_unref(udev);
+                }
+            }
+            return null;
+        }
+
+      
         
     }
 }
