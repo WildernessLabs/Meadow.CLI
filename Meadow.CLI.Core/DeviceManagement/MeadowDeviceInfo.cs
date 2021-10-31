3a1f8869bebe8bfda585cde98430813a9ab8f548
Mark Stevens <mark@thepcsite.co.uk>
2021-10-31 09:08:19 +0000

    Take "optional" values into consideration.

--- Meadow.CLI.Core/DeviceManagement/MeadowDeviceInfo.cs
+++ Meadow.CLI.Core/DeviceManagement/MeadowDeviceInfo.cs
@@ -5,36 +5,119 @@ namespace Meadow.CLI.Core.DeviceManagement
 {
     public class MeadowDeviceInfo
     {
-        /*
-        Meadow by Wilderness Labs,
-        Model: F7Micro,
-        MeadowOS Version: 0.1.0,
-        Processor: STM32F777IIK6,
-        Processor Id: 1d-00-29-00-12-51-36-30-33-33-37-33,
-        Serial Number: 3360335A3036,
-        CoProcessor: ESP32,
-        CoProcessor OS Version: 0.1.x\r\n"
-        */
+        /// <summary>
+        /// Name of the product (i.e. Meadow) key.
+        /// </summary>
+        private const string KN_PRODUCT = "product";
+
+        /// <summary>
+        /// Name of the model (e.g. F7V2) key.
+        /// </summary>
+        private const string KN_MODEL = "model";
+
+        /// <summary>
+        /// Name of the OS (Nuttx) version key.
+        /// </summary>
+        private const string KN_OS_VERSION = "osversion";
+
+        /// <summary>
+        /// Name of the processor type key.
+        /// </summary>
+        private const string KN_PROCESSOR_TYPE = "processortype";
+
+        /// <summary>
+        /// Name of the coprocessor type key.
+        /// </summary>
+        private const string KN_COPROCESSOR_TYPE = "coprocessortype";
+
+        /// <summary>
+        /// Name of the coprocessor version key.
+        /// </summary>
+        private const string KN_COPROCESSOR_VERSION = "coprocessorversion";
+
+        /// <summary>
+        /// Name of the mono version key.
+        /// </summary>
+        private const string KN_MONO_VERSION = "monoversion";
+
+        /// <summary>
+        /// Name of the processor ID key.
+        /// </summary>
+        private const string KN_PROCESSOR_ID = "processorid";
+
+        /// <summary>
+        /// Name of the hardware versiuon key.
+        /// </summary>
+        private const string KN_HARDWARE_VERSION = "hardware";
+
+        /// <summary>
+        /// Name of the device name key.
+        /// </summary>
+        private const string KN_DEVICE_NAME = "devicename";
+
+        /// <summary>
+        /// Name of the chip serial number key.
+        /// </summary>
+        private const string KN_SERIAL_NUMBER = "serialno";
+
+        /// <summary>
+        /// Name of the WiFi MAC address key.
+        /// </summary>
+        private const string KN_WIFI_MAC = "wifimac";
+
+        /// <summary>
+        /// Name of the soft access point MAC key
+        /// </summary>
+        private const string KN_SOFT_AP_MAC = "softapmac";
+
+        /// <summary>
+        /// String representation of an unknown MAC address.
+        /// </summary>
+        private const string UNKNOWN_MAC_ADDRESS = "00:00:00:00:00:00";
 
         /// <summary>
         /// Create a new device information object from the string returned from the board.
         /// </summary>
+        /// <remarks>
+        /// The device information is returned as a single string of key / value pairs.  The pairs will be delimetered
+        /// by the ~ character.  The key and value will be seperated by the | character.
+        /// </remarks>
         /// <param name="deviceInfoString">Full device information string returned from the board.</param>
         public MeadowDeviceInfo(string deviceInfoString)
         {
-            string[] elements = deviceInfoString.Split('~');
             _elements = new Dictionary<string, string>();
-
-            foreach (string element in elements)
+            if (deviceInfoString.Contains("~"))
             {
-                if (element.Length > 0)
+                //
+                //  Parse the post 0.6 device information string.
+                //
+                string[] elements = deviceInfoString.Split('~');
+
+                foreach (string element in elements)
                 {
-                    string[] keyValue = element.Split('|');
-                    if ((keyValue.Length == 2) && (keyValue[0].Length > 0) && (keyValue[1].Length > 0))
+                    if (element.Length > 0)
                     {
-                        _elements.Add(keyValue[0].ToLower(), keyValue[1]);
+                        string[] keyValue = element.Split('|');
+                        if ((keyValue.Length == 2) && (keyValue[0].Length > 0) && (keyValue[1].Length > 0))
+                        {
+                            _elements.Add(keyValue[0].ToLower(), keyValue[1]);
+                        }
                     }
                 }
+            }
+            else
+            {
+                //
+                //  Parse the pre OS version 0.6 device information string.
+                //
+                _elements.Add(KN_PRODUCT, deviceInfoString.Substring(0, deviceInfoString.IndexOf(' ')).Replace(" by Wilderness Labs", ""));
+                _elements.Add(KN_MODEL, ParseValue("Model: ", deviceInfoString));
+                _elements.Add(KN_OS_VERSION, ParseValue("MeadowOS Version: ", deviceInfoString));
+                _elements.Add(KN_PROCESSOR_TYPE, ParseValue("Processor: ", deviceInfoString));
+                _elements.Add(KN_PROCESSOR_ID, ParseValue("Processor Id:", deviceInfoString));
+                _elements.Add(KN_SERIAL_NUMBER, ParseValue("Serial Number: ", deviceInfoString));
+                _elements.Add(KN_COPROCESSOR_TYPE, ParseValue("CoProcessor: ", deviceInfoString));
+                _elements.Add(KN_COPROCESSOR_VERSION, ParseValue("CoProcessor OS Version: ", deviceInfoString));
             }
         }
 
@@ -46,67 +129,67 @@ namespace Meadow.CLI.Core.DeviceManagement
         /// <summary>
         /// Product description.
         /// </summary>
-        public string Name { get { return (_elements["product"]); } }
+        public string Product { get { return (_elements[KN_PRODUCT]); } }
 
         /// <summary>
         /// Model of the board.
         /// </summary>
-        public string Model { get { return (_elements["model"]); } }
+        public string Model { get { return (_elements[KN_MODEL]); } }
 
         /// <summary>
         /// OS version and build date.
         /// </summary>
-        public string MeadowOsVersion { get { return (_elements["osversion"]); } }
+        public string MeadowOsVersion { get { return (_elements[KN_OS_VERSION]); } }
 
         /// <summary>
         /// Type of processor on the board.
         /// </summary>
-        public string Processor { get { return (_elements["processortype"]); } }
+        public string ProcessorType { get { return (_elements[KN_PROCESSOR_TYPE]); } }
 
         /// <summary>
         /// Type of coprocessor on the board.
         /// </summary>
-        public string CoProcessor { get { return (_elements["coprocessortype"]); } }
+        public string CoProcessorType { get { return (_elements[KN_COPROCESSOR_TYPE]); } }
 
         /// <summary>
         /// Coprocessor firmware version.
         /// </summary>
-        public string CoProcessorOs { get { return ValueOrDefault("coprocessorversion", "Not available"); } }
+        public string CoProcessorOsVersion { get { return ValueOrDefault(KN_COPROCESSOR_VERSION, "Not available"); } }
 
         /// <summary>
         /// Version of Mono deployed on the board.
         /// </summary>
-        public string MonoVersion { get { return ValueOrDefault("monoversion", "Not available"); } }
+        public string MonoVersion { get { return ValueOrDefault(KN_MONO_VERSION, "Not available"); } }
 
         /// <summary>
         /// ID of the STM32 processor.
         /// </summary>
-        public string ProcessorId { get { return (_elements["processorid"]); } }
+        public string ProcessorId { get { return ValueOrDefault(KN_PROCESSOR_ID, string.Empty); } }
 
         /// <summary>
         /// Hardware version.
         /// </summary>
-        public string HardwareVersion { get { return (_elements["hardware"]); } }
+        public string HardwareVersion { get { return ValueOrDefault(KN_HARDWARE_VERSION, string.Empty); } }
 
         /// <summary>
         /// Name of the device from the config file (or the default value when no config file is on the board).
         /// </summary>
-        public string DeviceName { get { return (_elements["devicename"]); } }
+        public string DeviceName { get { return ValueOrDefault(KN_DEVICE_NAME, string.Empty); } }
 
         /// <summary>
         /// STM32 serial number.
         /// </summary>
-        public string SerialNumber { get { return (_elements["serialno"]); } }
+        public string SerialNumber { get { return ValueOrDefault(KN_SERIAL_NUMBER, string.Empty); } }
 
         /// <summary>
         /// MAC address of the coprocessor.
         /// </summary>
-        public string WiFiMacAddress { get { return ValueOrDefault("wifimac", string.Empty); } }
+        public string WiFiMacAddress { get { return ValueOrDefault(KN_WIFI_MAC, string.Empty); } }
 
         /// <summary>
         /// MAC address of the coprocessor when it is used as an access point.
         /// </summary>
-        public string SoftApMacAddress { get { return ValueOrDefault("softapmac", string.Empty); } }
+        public string SoftApMacAddress { get { return ValueOrDefault(KN_SOFT_AP_MAC, string.Empty); } }
 
         /// <summary>
         /// Look up the specified key in the dictionary of values returned by the hardware.
@@ -129,31 +212,98 @@ namespace Meadow.CLI.Core.DeviceManagement
         }
 
         /// <summary>
+        /// Create part of the device information string depending upon the presence (or not) of one
+        /// of the device information components.
+        /// </summary>
+        /// <param name="name">Name (description) that will appear in the device information output.</param>
+        /// <param name="value">Value to to added (this may be an empty string).</param>
+        /// <returns>String (possibly empty) to be added to the device information output.</returns>
+        private string AddOptionalValue(string name, string value)
+        {
+            string result = string.Empty;
+
+            if (!string.IsNullOrEmpty(value))
+            {
+                result = $"{name} {value}";
+            }
+            return (result);
+        }
+
+        /// <summary>
         /// Generate a meaningful description of the hardware.
         /// </summary>
+        /// <remarks>
+        /// Format of the message:
+        /// 
+        /// Meadow by Wilderness Labs
+        /// Board Information - Model: F7Micro, Hardware version: F7v2, Device name: MeadowF7V2
+        /// Hardware Information - Processor type: STM32F777IIK6, ID: 32-00-1d-00-11-51-36-30-33-33-37-33, Serial number: 3354336F3036, Coprocessor type: ESP32
+        /// Firmware Versions - OS: 0.5.3.0 (Oct 30 2021 10:21:54) Mono: 0.5.3.0, Coprocessor: 0.5.3.0
+        /// MAC Address(es) - WiFi: 94:B9:7E:91:0F:78
+        /// 
+        /// </remarks>
         /// <returns>Meaningful description of the hardware.</returns>
         public override string ToString()
         {
             string deviceInfo;
-            string element;
 
-            deviceInfo = $"{Name}\n";
-            deviceInfo += $"Board Information - Model: {Model}, Hardware version: {HardwareVersion}, Device name: {DeviceName}\n";
-            deviceInfo += $"Hardware Information - Processor type: {Processor}, ID: {ProcessorId}, Serial number: {SerialNumber}, Coprocessor type: {CoProcessor}\n";
-            deviceInfo += $"Firmware Versions - OS: {MeadowOsVersion} Mono: {MonoVersion}, Coprocessor: {CoProcessorOs}\n";
-            if (!string.IsNullOrEmpty(WiFiMacAddress) || !string.IsNullOrEmpty(SoftApMacAddress))
+            if (Product.Contains(" by Wilderness Labs"))
             {
-                deviceInfo += "MAC Address(es) - ";
-                if (!string.IsNullOrEmpty(WiFiMacAddress))
+                deviceInfo = $"{Product}\n";
+            }
+            else
+            {
+                deviceInfo = $"{Product} by Wilderness Labs\n";
+            }
+            deviceInfo += $"Board Information - Model: {Model}";
+            deviceInfo += AddOptionalValue(", Hardware version:", HardwareVersion);
+            deviceInfo += AddOptionalValue(", Device name:", DeviceName);
+            deviceInfo += "\n";
+            deviceInfo += $"Hardware Information - Processor type: {ProcessorType}";
+            deviceInfo += AddOptionalValue(", ID:", ProcessorId);
+            deviceInfo += AddOptionalValue(", Serial number:", SerialNumber);
+            deviceInfo += $", Coprocessor type: {CoProcessorType}\n";
+            deviceInfo += $"Firmware Versions - OS: {MeadowOsVersion} Mono: {MonoVersion}, Coprocessor: {CoProcessorOsVersion}";
+
+            string macAddresses = string.Empty;
+            int macCount = 0;
+            if (!string.IsNullOrEmpty(WiFiMacAddress) && WiFiMacAddress != UNKNOWN_MAC_ADDRESS)
+            {
+                macCount++;
+                macAddresses += $"WiFi: {WiFiMacAddress} ";
+            }
+            if (!string.IsNullOrEmpty(SoftApMacAddress) && SoftApMacAddress != UNKNOWN_MAC_ADDRESS)
+            {
+                macCount++;
+                macAddresses += $"AP: {SoftApMacAddress} ";
+            }
+            if (macCount > 0)
+            {
+                deviceInfo += "\n";
+                if (macCount > 1)
                 {
-                    deviceInfo += $"WiFi: {WiFiMacAddress} ";
+                    deviceInfo += "MAC Addresses: ";
                 }
-                if (!string.IsNullOrEmpty(SoftApMacAddress) && (SoftApMacAddress != "00:00:00:00:00:00"))
+                else
                 {
-                    deviceInfo += $"AP: {SoftApMacAddress}";
+                    deviceInfo += "MAC Address: ";
                 }
+                deviceInfo += $"{macAddresses}";
             }
             return (deviceInfo);
+        }
+
+        /// <summary>
+        /// Parse the device information string for a key and extract the value.
+        /// </summary>
+        /// <param name="key">Key to be searched for.</param>
+        /// <param name="source">Device information string.</param>
+        /// <returns>Value associated with the key.</returns>
+        private string ParseValue(string key, string source)
+        {
+            var start = source.IndexOf(key, StringComparison.Ordinal) + key.Length;
+            var end = source.IndexOf(',', start);
+            return source.Substring(start, end - start);
         }
     }
 }
