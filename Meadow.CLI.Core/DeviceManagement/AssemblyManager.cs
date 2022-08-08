638a7ee4e64f3e85336f30eceb28656512c5d82f
Alexis Christoforides <alexis@thenull.net>
2022-08-08 05:02:04 -0400

    Implement minimum-codesize linker via the Mono IL Linker. (#176)
    * Implement minimum-codesize linker via the Mono IL Linker.
    
    Deployment assemblies go through an extra processing step now, to remove unused code and save resources.
    
    * Add Meadow.Logging to meadow_link.xml
    
    * Move resource dlls from the CLI to CLI.Core and bump version 0.19.5
    
    * bump version to 0.19.5
    
    * Rename resources folder to Resources step 1
    
    * rename resources folder to Resources step 2
    
    * Safer path management in AssemblyManager and a few parens
    
    * CLI.Core csproj cleanup and update resources folder to Resources
    
    * Simplify linker console output
    
    * Add x86 and x64 Debug and Release Configurations, so that VS_Mac extension builds.
    
    * Make LinkDependencies awaitable.
    
    * Remove block that stopped the illinker from being included in the package.
    
    * Use StreamReader for both StandardOutput and StandardError and write out its contents.
    
    Co-authored-by: Adrian Stevens <adrian.stevens@gmail.com>
    Co-authored-by: Dominique Louis <savagesoftware@gmail.com>

--- Meadow.CLI.Core/DeviceManagement/AssemblyManager.cs
+++ Meadow.CLI.Core/DeviceManagement/AssemblyManager.cs
@@ -2,6 +2,10 @@
 using System.IO;
 using Mono.Cecil;
 using Mono.Collections.Generic;
+using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Threading.Tasks;
 
 namespace Meadow.CLI.Core.DeviceManagement
 {
@@ -12,6 +16,82 @@ namespace Meadow.CLI.Core.DeviceManagement
         private static readonly List<string> dependencyMap = new();
 
         private static string? meadowAssembliesPath = null;
+
+        public static async Task<IEnumerable<string>> LinkDependencies (string file, string path, List<string> dependencies, bool includePdbs)
+        {
+            var prelink_dir = Path.Combine (path, "prelink_bin");
+            var prelink_app = Path.Combine (prelink_dir, file);
+            var prelink_os = Path.Combine (prelink_dir, "Meadow.dll");
+
+            if (Directory.Exists (prelink_dir)) {
+                Directory.Delete (prelink_dir, recursive: true);
+            }
+
+            Directory.CreateDirectory (prelink_dir);
+            File.Copy (Path.Combine (path, file), prelink_app, overwrite: true);
+
+            foreach (var dependency in dependencies) {
+                File.Copy (dependency,
+                            Path.Combine (prelink_dir, Path.GetFileName (dependency)),
+                            overwrite: true);
+
+                if (includePdbs) {
+                    var pdbFile = Path.ChangeExtension (dependency, "pdb");
+                    if (File.Exists (pdbFile))
+                        File.Copy (pdbFile,
+                            Path.Combine (prelink_dir, Path.GetFileName (pdbFile)),
+                            overwrite: true);
+                }
+            }
+
+            var postlink_dir = Path.Combine (path, "postlink_bin");
+
+            if (Directory.Exists (postlink_dir)) {
+                Directory.Delete (postlink_dir, recursive: true);
+            }
+            Directory.CreateDirectory (postlink_dir);
+
+            var base_path = Path.GetDirectoryName (Assembly.GetExecutingAssembly ().Location);
+            var illinker_path = Path.Combine (base_path, "Resources", "illink.dll");
+            var descriptor_path = Path.Combine (base_path, "Resources", "meadow_link.xml");
+
+            var monolinker_args = $"\"{illinker_path}\" -x \"{descriptor_path}\" --skip-unresolved --deterministic --keep-facades true --ignore-descriptors true -b true -c link -o \"{postlink_dir}\" -r \"{prelink_app}\" -a \"{prelink_os}\" -d \"{prelink_dir}\"";
+
+            Console.WriteLine ("Trimming assemblies to reduce size (may take several seconds)...");
+
+            using (var process = new Process ()) {
+                process.StartInfo.FileName = "dotnet";
+                process.StartInfo.Arguments = monolinker_args;
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.CreateNoWindow = true;
+                process.StartInfo.RedirectStandardError = true;
+                process.StartInfo.RedirectStandardOutput = true;
+                process.Start ();
+
+                // To avoid deadlocks, read the output stream first and then wait
+                string stdOutReaderResult;
+                using (StreamReader stdOutReader = process.StandardOutput) {
+                    stdOutReaderResult = await stdOutReader.ReadToEndAsync ();
+                    Console.WriteLine ("StandardOutput Contains: " + stdOutReaderResult);
+                }
+
+                string stdErrorReaderResult;
+                using (StreamReader stdErrorReader = process.StandardError) {
+                    stdErrorReaderResult = await stdErrorReader.ReadToEndAsync ();
+                    Console.WriteLine ("StandardError Contains: " + stdErrorReaderResult);
+                }
+
+                process.WaitForExit (60000);
+                if (process.ExitCode != 0) {
+                    // TODO when Debugging throw new Exception ($"Trimming failed - ILLinker execution error!\nProcess Info: {process.StartInfo.FileName} {process.StartInfo.Arguments} \nExit Code: {process.ExitCode}");
+                    throw new Exception ($"Trimming failed - ILLinker execution error!\nExit Code: {process.ExitCode}");
+                }
+            }
+
+            Console.WriteLine ("Trimming complete");
+
+            return Directory.EnumerateFiles (postlink_dir);
+        }
 
         public static List<string> GetDependencies(string file, string path, string osVersion)
         {
