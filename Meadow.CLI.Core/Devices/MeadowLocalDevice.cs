ea3eec61ef238af85fbce2e9d15440c5c4775d92
Dominique Louis <savagesoftware@gmail.com>
2023-03-29 17:48:24 +0100

    Revert timeout back to 60s.

--- Meadow.CLI.Core/Devices/MeadowLocalDevice.cs
+++ Meadow.CLI.Core/Devices/MeadowLocalDevice.cs
@@ -15,7 +15,7 @@ namespace Meadow.CLI.Core.Devices
 {
     public abstract partial class MeadowLocalDevice : IMeadowDevice
     {
-        private protected TimeSpan DefaultTimeout = TimeSpan.FromSeconds(120);
+        private protected TimeSpan DefaultTimeout = TimeSpan.FromSeconds(60);
 
         public ILogger? Logger { get; }
         public MeadowDataProcessor DataProcessor { get; }
