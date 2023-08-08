112c1dc6ecb4c3604e9a90bde8920a704999f8b7
Dominique Louis <savagesoftware@gmail.com>
2023-08-08 11:42:53 +0100

    Use old style namespaces for compatibility, for now.

--- Meadow.CLI/Commands/Cloud/Collection/ListCollectionCommand.cs
+++ Meadow.CLI/Commands/Cloud/Collection/ListCollectionCommand.cs
@@ -9,78 +9,79 @@ using Meadow.CLI.Core.Exceptions;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace Meadow.CLI.Commands.Cloud.Collection;
-
-public class ListCollectionCommand
+namespace Meadow.CLI.Commands.Cloud.Collection
 {
-    [Command("collection list", Description = "List Meadow Collections")]
-    public class ListCommand : ICommand
+    public class ListCollectionCommand
     {
-        private readonly ILogger<LogoutCommand> _logger;
-        UserService _userService;
-        private CollectionService _collectionService;
-        IConfiguration _config;
-
-        public ListCommand(ILoggerFactory loggerFactory, UserService userService, CollectionService collectionService,
-            IConfiguration config)
+        [Command("collection list", Description = "List Meadow Collections")]
+        public class ListCommand : ICommand
         {
-            _logger = loggerFactory.CreateLogger<LogoutCommand>();
-            _userService = userService;
-            _collectionService = collectionService;
-            _config = config;
-        }
+            private readonly ILogger<LogoutCommand> _logger;
+            UserService _userService;
+            private CollectionService _collectionService;
+            IConfiguration _config;
 
-        [CommandOption("orgId", 'o', Description = "Organization Id", IsRequired = false)]
-        public string OrgId { get; set; }
-        [CommandOption("host", Description = "Optionally set a host (default is https://www.meadowcloud.co)", IsRequired = false)]
-        public string Host { get; set; }
-
-        public async ValueTask ExecuteAsync(IConsole console)
-        {
-            var cancellationToken = console.RegisterCancellationHandler();
-
-            await Task.Yield();
-
-            try
+            public ListCommand(ILoggerFactory loggerFactory, UserService userService, CollectionService collectionService,
+                IConfiguration config)
             {
-                var userOrgs = await _userService.GetUserOrgs(Host, cancellationToken).ConfigureAwait(false);
-                if (!userOrgs.Any())
-                {
-                    _logger.LogInformation($"Please visit {_config[Constants.MEADOW_CLOUD_HOST_CONFIG_NAME]} to register your account.");
-                    return;
-                }
-                else if (userOrgs.Count() > 1 && string.IsNullOrEmpty(OrgId))
-                {
-                    _logger.LogInformation($"Please specify the orgId.");
-                    return;
-                }
-                else if (userOrgs.Count() == 1 && string.IsNullOrEmpty(OrgId))
-                {
-                    OrgId = userOrgs.First().Id;
-                }
-
-                if (!userOrgs.Select(x => x.Id).Contains(OrgId))
-                {
-                    _logger.LogInformation($"Invalid orgId: {OrgId}");
-                    return;
-                }
-            }
-            catch (MeadowCloudAuthException)
-            {
-                _logger.LogInformation($"You must be signed in to execute this command.");
-                return;
-            }
-            
-            var collections = await _collectionService.GetOrgCollections(OrgId, Host, cancellationToken);
-
-            if (collections == null || collections.Count() == 0)
-            {
-                _logger.LogInformation("No collections found.");
+                _logger = loggerFactory.CreateLogger<LogoutCommand>();
+                _userService = userService;
+                _collectionService = collectionService;
+                _config = config;
             }
 
-            foreach (var collection in collections)
+            [CommandOption("orgId", 'o', Description = "Organization Id", IsRequired = false)]
+            public string OrgId { get; set; }
+            [CommandOption("host", Description = "Optionally set a host (default is https://www.meadowcloud.co)", IsRequired = false)]
+            public string Host { get; set; }
+
+            public async ValueTask ExecuteAsync(IConsole console)
             {
-                _logger.LogInformation($"{collection.Id} | {collection.Name}");
+                var cancellationToken = console.RegisterCancellationHandler();
+
+                await Task.Yield();
+
+                try
+                {
+                    var userOrgs = await _userService.GetUserOrgs(Host, cancellationToken).ConfigureAwait(false);
+                    if (!userOrgs.Any())
+                    {
+                        _logger.LogInformation($"Please visit {_config[Constants.MEADOW_CLOUD_HOST_CONFIG_NAME]} to register your account.");
+                        return;
+                    }
+                    else if (userOrgs.Count() > 1 && string.IsNullOrEmpty(OrgId))
+                    {
+                        _logger.LogInformation($"Please specify the orgId.");
+                        return;
+                    }
+                    else if (userOrgs.Count() == 1 && string.IsNullOrEmpty(OrgId))
+                    {
+                        OrgId = userOrgs.First().Id;
+                    }
+
+                    if (!userOrgs.Select(x => x.Id).Contains(OrgId))
+                    {
+                        _logger.LogInformation($"Invalid orgId: {OrgId}");
+                        return;
+                    }
+                }
+                catch (MeadowCloudAuthException)
+                {
+                    _logger.LogInformation($"You must be signed in to execute this command.");
+                    return;
+                }
+
+                var collections = await _collectionService.GetOrgCollections(OrgId, Host, cancellationToken);
+
+                if (collections == null || collections.Count() == 0)
+                {
+                    _logger.LogInformation("No collections found.");
+                }
+
+                foreach (var collection in collections)
+                {
+                    _logger.LogInformation($"{collection.Id} | {collection.Name}");
+                }
             }
         }
     }
