d835dba5fe8660e0c81d3c68a06607174da72762
Dominique Louis <savagesoftware@gmail.com>
2023-10-13 20:48:22 +0100

    Refactor Trace commands to have a common base.

---
+++ Source/v2/Meadow.Cli/Commands/Current/Trace/BaseTraceEnableCommand.cs
@@ -0,0 +1,28 @@
+﻿using Meadow.Hcom;
+using Microsoft.Extensions.Logging;
+
+namespace Meadow.CLI.Commands.DeviceManagement;
+
+public abstract class BaseTraceEnableCommand<T> : BaseDeviceCommand<T>
+{
+    protected IMeadowConnection? Connection { get; private set; }
+
+    public BaseTraceEnableCommand(MeadowConnectionManager connectionManager, ILoggerFactory loggerFactory)
+        : base(connectionManager, loggerFactory)
+    {
+    }
+
+    protected override async ValueTask ExecuteCommand()
+    {
+        Connection = await GetCurrentConnection();
+
+        if (Connection != null)
+        {
+            Connection.DeviceMessageReceived += (s, e) =>
+            {
+                Logger?.LogInformation(e.message);
+            };
+        }
+    }
+}
+
--- Source/v2/Meadow.Cli/Commands/Current/Trace/TraceDisableCommand.cs
+++ Source/v2/Meadow.Cli/Commands/Current/Trace/TraceDisableCommand.cs
@@ -4,7 +4,7 @@ using Microsoft.Extensions.Logging;
 namespace Meadow.CLI.Commands.DeviceManagement;
 
 [Command("trace disable", Description = "Disable trace logging on the Meadow")]
-public class TraceDisableCommand : BaseDeviceCommand<TraceDisableCommand>
+public class TraceDisableCommand : BaseTraceEnableCommand<TraceDisableCommand>
 {
     public TraceDisableCommand(MeadowConnectionManager connectionManager, ILoggerFactory loggerFactory)
         : base(connectionManager, loggerFactory)
@@ -13,21 +13,13 @@ public class TraceDisableCommand : BaseDeviceCommand<TraceDisableCommand>
 
     protected override async ValueTask ExecuteCommand()
     {
-        var connection = await GetCurrentConnection();
+        await base.ExecuteCommand();
 
-        if (connection == null)
+        if (Connection != null && Connection.Device != null)
         {
-            return;
+            Logger?.LogInformation("Disabling tracing...");
+
+            await Connection.Device.TraceDisable(CancellationToken);
         }
-
-        connection.DeviceMessageReceived += (s, e) =>
-        {
-            Logger?.LogInformation(e.message);
-        };
-
-        Logger?.LogInformation("Disabling tracing...");
-
-        await connection.Device.TraceDisable(CancellationToken);
     }
 }
-
