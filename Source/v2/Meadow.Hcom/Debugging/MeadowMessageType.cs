2a7272a949ee1cbf9c4e436f51fb8759a99af714
Dominique Louis <savagesoftware@gmail.com>
2023-10-14 23:26:01 +0100

    Remove warnings from Hcom Debugging server.

---
+++ Source/v2/Meadow.Hcom/Debugging/MeadowMessageType.cs
@@ -0,0 +1,23 @@
+﻿namespace Meadow.Hcom;
+
+// For data received due to a CLI request these provide a secondary
+// type of identification. The primary being the protocol request value
+public enum MeadowMessageType
+{
+    AppOutput,
+    ErrOutput,
+    DeviceInfo,
+    FileListTitle,
+    FileListMember,
+    FileListCrcMember,
+    Data,
+    InitialFileData,
+    MeadowTrace,
+    SerialReconnect,
+    Accepted,
+    Concluded,
+    DownloadStartOkay,
+    DownloadStartFail,
+    DownloadFailed,
+    DevicePublicKey
+}
