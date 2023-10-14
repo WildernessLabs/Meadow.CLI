4f0d5a5fc1f85441cfbb911f7d2779f55f8883d8
Dominique Louis <savagesoftware@gmail.com>
2023-10-14 22:43:35 +0100

    Remove warnings from Config Command.

--- Source/v2/Meadow.Cli/Commands/Current/Config/ConfigCommand.cs
+++ Source/v2/Meadow.Cli/Commands/Current/Config/ConfigCommand.cs
@@ -24,45 +24,48 @@ public class ConfigCommand : BaseSettingsCommand<ConfigCommand>
 
     protected override async ValueTask ExecuteCommand()
     {
-        if (List)
+        await Task.Run(() =>
         {
-            Logger?.LogInformation($"Current CLI configuration");
-
-            // display all current config
-            var settings = SettingsManager.GetPublicSettings();
-            if (settings.Count == 0)
+            if (List)
             {
-                Logger?.LogInformation($"  <no settings found>");
+                Logger?.LogInformation($"Current CLI configuration");
+
+                // display all current config
+                var settings = SettingsManager.GetPublicSettings();
+                if (settings.Count == 0)
+                {
+                    Logger?.LogInformation($"  <no settings found>");
+                }
+                else
+                {
+                    foreach (var kvp in SettingsManager.GetPublicSettings())
+                    {
+                        Logger?.LogInformation($"  {kvp.Key} = {kvp.Value}");
+                    }
+                }
             }
             else
             {
-                foreach (var kvp in SettingsManager.GetPublicSettings())
+                switch (Settings?.Length)
                 {
-                    Logger?.LogInformation($"  {kvp.Key} = {kvp.Value}");
+                    case 0:
+                        // not valid
+                        throw new CommandException($"No setting provided");
+                    case 1:
+                        // erase a setting
+                        Logger?.LogInformation($"{Environment.NewLine}Deleting Setting {Settings[0]}");
+                        SettingsManager.DeleteSetting(Settings[0]);
+                        break;
+                    case 2:
+                        // set a setting
+                        Logger?.LogInformation($"{Environment.NewLine}Setting {Settings[0]}={Settings[1]}");
+                        SettingsManager.SaveSetting(Settings[0], Settings[1]);
+                        break;
+                    default:
+                        // not valid;
+                        throw new CommandException($"Too many parameters provided");
                 }
             }
-        }
-        else
-        {
-            switch (Settings?.Length)
-            {
-                case 0:
-                    // not valid
-                    throw new CommandException($"No setting provided");
-                case 1:
-                    // erase a setting
-                    Logger?.LogInformation($"{Environment.NewLine}Deleting Setting {Settings[0]}");
-                    SettingsManager.DeleteSetting(Settings[0]);
-                    break;
-                case 2:
-                    // set a setting
-                    Logger?.LogInformation($"{Environment.NewLine}Setting {Settings[0]}={Settings[1]}");
-                    SettingsManager.SaveSetting(Settings[0], Settings[1]);
-                    break;
-                default:
-                    // not valid;
-                    throw new CommandException($"Too many parameters provided");
-            }
-        }
+        });
     }
 }
