eb61f8022c9614c055d86a4f8f4fd0c7a42cb372
Dominique Louis <savagesoftware@gmail.com>
2023-10-21 18:57:22 +0100

    Add Assembly Version number so that --version displayt the correct version.

---
+++ Source/v2/Meadow.Cli/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+[assembly: System.Reflection.AssemblyFileVersion(Meadow.CLI.Constants.CLI_VERSION)]
+[assembly: System.Reflection.AssemblyVersion(Meadow.CLI.Constants.CLI_VERSION)]
+[assembly: System.Reflection.AssemblyInformationalVersion(Meadow.CLI.Constants.CLI_VERSION)]
+
+namespace Meadow.CLI
+{
+    public static class Constants
+    {
+        public const string CLI_VERSION = "2.0.0.0";
+    }
+}
