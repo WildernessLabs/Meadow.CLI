dee89d1b94d12616d0267fa29d2c873b75ed22b7
Dominique Louis <savagesoftware@gmail.com>
2023-10-26 16:36:23 +0100

    Make ColourConsoleText an extension method. Much more elegant.

--- Source/v2/Meadow.Cli/Properties/AssemblyInfo.cs
+++ Source/v2/Meadow.Cli/Properties/AssemblyInfo.cs
@@ -7,30 +7,29 @@ namespace Meadow.CLI
     public static class Constants
     {
         public const string CLI_VERSION = "2.0.0.0";
+    }
 
-        public const string ConsoleColourRed = "\u001b[31m";
-        public const string ConsoleColourGreen = "\u001b[32m";
-        public const string ConsoleColourYellow = "\u001b[33m";
+    public static class StringExtensions
+    {
+        public const string ConsoleColourBlack = "\u001b[30m";
         public const string ConsoleColourBlue = "\u001b[34m";
-        public const string ConsoleColourClear = "\u001b[0m";
+        public const string ConsoleColourCyan = "\u001b[36m";
+        public const string ConsoleColourGreen = "\u001b[32m";
+        public const string ConsoleColourMagenta = "\u001b[35m";
+        public const string ConsoleColourRed = "\u001b[31m";
+        public const string ConsoleColourReset = "\u001b[0m";
+        public const string ConsoleColourWhite = "\u001b[37m";
+        public const string ConsoleColourYellow = "\u001b[33m";
 
-        public static string ColourConsoleText(string? textColour, string? textToColour)
+        public static string ColourConsoleText(this string textToColour, string textColour)
         {
-            if (!string.IsNullOrEmpty(textColour)
-                && !string.IsNullOrEmpty(textToColour))
+            if (!string.IsNullOrEmpty(textToColour))
             {
-                return textColour + textToColour + ConsoleColourClear;
+                return textColour + textToColour + ConsoleColourReset;
             }
             else
             {
-                if (!string.IsNullOrEmpty(textToColour))
-                {
-                    return textToColour;
-                }
-                else
-                {
-                    return string.Empty;
-                }
+                return string.Empty;
             }
         }
     }
