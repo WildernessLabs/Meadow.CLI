33bff8a43f1046a1c847d3e900edd42e1eaed846
Dominique Louis <savagesoftware@gmail.com>
2023-10-26 12:22:48 +0100

    Remove duplicate message during meadow listen. Also colour output.

--- Source/v2/Meadow.Cli/Properties/AssemblyInfo.cs
+++ Source/v2/Meadow.Cli/Properties/AssemblyInfo.cs
@@ -7,5 +7,31 @@ namespace Meadow.CLI
     public static class Constants
     {
         public const string CLI_VERSION = "2.0.0.0";
+
+        public const string ConsoleColourRed = "\u001b[31m";
+        public const string ConsoleColourGreen = "\u001b[32m";
+        public const string ConsoleColourYellow = "\u001b[33m";
+        public const string ConsoleColourBlue = "\u001b[34m";
+        public const string ConsoleColourClear = "\u001b[0m";
+
+        public static string ColourConsoleText(string? textColour, string? textToColour)
+        {
+            if (!string.IsNullOrEmpty(textColour)
+                && !string.IsNullOrEmpty(textToColour))
+            {
+                return textColour + textToColour + ConsoleColourClear;
+            }
+            else
+            {
+                if (!string.IsNullOrEmpty(textToColour))
+                {
+                    return textToColour;
+                }
+                else
+                {
+                    return string.Empty;
+                }
+            }
+        }
     }
 }
