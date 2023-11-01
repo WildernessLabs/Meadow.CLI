3c1772ff86330d354ab2fb16ca7340de3ab2811a
Dominique Louis <savagesoftware@gmail.com>
2023-11-01 17:20:28 +0000

    Remove duplicate extension methods.

--- Source/v2/Meadow.Cli/Properties/AssemblyInfo.cs
+++ Source/v2/Meadow.Cli/Properties/AssemblyInfo.cs
@@ -8,29 +8,4 @@ namespace Meadow.CLI
     {
         public const string CLI_VERSION = "2.0.0.0";
     }
-
-    public static class StringExtensions
-    {
-        public const string ConsoleColourBlack = "\u001b[30m";
-        public const string ConsoleColourBlue = "\u001b[34m";
-        public const string ConsoleColourCyan = "\u001b[36m";
-        public const string ConsoleColourGreen = "\u001b[32m";
-        public const string ConsoleColourMagenta = "\u001b[35m";
-        public const string ConsoleColourRed = "\u001b[31m";
-        public const string ConsoleColourReset = "\u001b[0m";
-        public const string ConsoleColourWhite = "\u001b[37m";
-        public const string ConsoleColourYellow = "\u001b[33m";
-
-        public static string ColourConsoleText(this string textToColour, string textColour)
-        {
-            if (!string.IsNullOrEmpty(textToColour))
-            {
-                return textColour + textToColour + ConsoleColourReset;
-            }
-            else
-            {
-                return string.Empty;
-            }
-        }
-    }
 }
