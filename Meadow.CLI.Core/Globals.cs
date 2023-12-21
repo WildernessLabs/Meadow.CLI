9a67a3fbff0922a1e83c6803874b14ad183c13ed
CartBlanche <savagesoftware@gmail.com>
2023-12-21 12:45:24 +0000

    Remove VS2019 if def

--- Meadow.CLI.Core/Globals.cs
+++ Meadow.CLI.Core/Globals.cs
@@ -1,6 +1,2 @@
-﻿#if VS2019
-global using Meadow.CLI.Core.Logging;
-#else
-global using Microsoft.Extensions.Logging;
+﻿global using Microsoft.Extensions.Logging;
 global using Microsoft.Extensions.Logging.Abstractions;
-#endif
