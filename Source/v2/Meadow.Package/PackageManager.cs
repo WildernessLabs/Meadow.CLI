6c167881b5bfa9bd1ccb3a87eb9e7be4bfa9952d
doingnz <richard.scott@healthsystemslimited.com>
2024-02-10 15:41:54 +1300

    Add support to set created mpak file name from CLI
    There is an existsing -n opti0on that sets the property MpakPath. This value needs to be passed into the method creating the mpak. Default value to null which will t hen automatically assign file name using DateTime.

--- Source/v2/Meadow.Package/PackageManager.cs
+++ Source/v2/Meadow.Package/PackageManager.cs
@@ -163,10 +163,10 @@ public partial class PackageManager : IPackageManager
 
     public const string PackageMetadataFileName = "info.json";
 
-    public Task<string> AssemblePackage(
-        string contentSourceFolder,
+    public Task<string> AssemblePackage(string contentSourceFolder,
         string outputFolder,
         string osVersion,
+        string? mpakName = null,
         string filter = "*",
         bool overwrite = false,
         CancellationToken? cancellationToken = null)
@@ -177,7 +177,8 @@ public partial class PackageManager : IPackageManager
             di.Create();
         }
 
-        var mpakName = Path.Combine(outputFolder, $"{DateTime.UtcNow:yyyyMMddff}.mpak");
+        // uncomment to force ".mpak" extension. mpakName = Path.ChangeExtension(mpakName, ".mpak");
+        mpakName = Path.Combine(outputFolder, string.IsNullOrWhiteSpace(mpakName) ? $"{DateTime.UtcNow:yyyyMMddff}.mpak" : mpakName);
 
         if (File.Exists(mpakName))
         {
