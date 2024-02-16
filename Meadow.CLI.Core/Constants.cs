17d60fb15f26703158c21ccb47352c70195d5b66
Dominique Louis <savagesoftware@gmail.com>
2024-02-16 23:25:48 +0000

    make v1 CLI v1.9.0

--- Meadow.CLI.Core/Constants.cs
+++ Meadow.CLI.Core/Constants.cs
@@ -7,7 +7,7 @@ namespace Meadow.CLI.Core
 {
     public static class Constants
     {
-        public const string CLI_VERSION = "2.0.6.0";
+        public const string CLI_VERSION = "1.9.0.0";
         public const ushort HCOM_PROTOCOL_PREVIOUS_VERSION_NUMBER = 0x0007;
         public const ushort HCOM_PROTOCOL_CURRENT_VERSION_NUMBER = 0x0008;         // Used for transmission
         public const string WILDERNESS_LABS_USB_VID = "2E6A";
