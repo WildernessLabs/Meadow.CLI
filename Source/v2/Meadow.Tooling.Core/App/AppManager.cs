5c85882d75a04ae773669049e0f70150c56147a4
Dominique Louis <savagesoftware@gmail.com>
2024-03-25 12:59:40 +0000

    Fix copy paste bug :facepalm:

--- Source/v2/Meadow.Tooling.Core/App/AppManager.cs
+++ Source/v2/Meadow.Tooling.Core/App/AppManager.cs
@@ -175,7 +175,7 @@ public static class AppManager
         {
             logger?.LogInformation($"Enabling runtime...");
 
-            await meadowConnection.RuntimeDisable(cancellationToken);
+            await meadowConnection.RuntimeEnable(cancellationToken);
         }
 
         // Wait for the device to realise it's own existence.
