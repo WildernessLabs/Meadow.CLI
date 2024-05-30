4f87e6f61c0dec33e701e234b176f58309f19b9d
Adrian Stevens <adrian.stevens@gmail.com>
2024-05-30 16:23:56 -0700

    Add NSH enable and disable commands

---
+++ Source/v2/Meadow.CLI/Commands/Current/Nsh/NshEnableCommand.cs
@@ -0,0 +1,25 @@
+﻿using CliFx.Attributes;
+using Microsoft.Extensions.Logging;
+
+namespace Meadow.CLI.Commands.DeviceManagement;
+
+[Command("nsh enable", Description = "Enables the Nuttx shell on the Meadow device")]
+public class NshEnableCommand : BaseDeviceCommand<RuntimeEnableCommand>
+{
+    public NshEnableCommand(MeadowConnectionManager connectionManager, ILoggerFactory loggerFactory)
+        : base(connectionManager, loggerFactory)
+    { }
+
+    protected override async ValueTask ExecuteCommand()
+    {
+        var device = await GetCurrentDevice();
+
+        Logger?.LogInformation("Enabling NSH...");
+
+        var state = await device.IsRuntimeEnabled(CancellationToken);
+
+        await device.NshEnable(CancellationToken);
+
+        Logger?.LogInformation("NSH enabled");
+    }
+}
---
+++ Source/v2/Meadow.Hcom/SerialRequests/NshEnableDisableRequest.cs
@@ -0,0 +1,6 @@
+﻿namespace Meadow.Hcom;
+
+internal class NshEnableDisableRequest : Request
+{
+    public override RequestType RequestType => RequestType.HCOM_MDOW_REQUEST_ENABLE_DISABLE_NSH;
+}
