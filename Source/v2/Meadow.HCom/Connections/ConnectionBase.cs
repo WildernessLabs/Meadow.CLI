7a542b1d31c018c4629f1dafca79cf6b1e1f13fb
Adrian Stevens <adrian.stevens@gmail.com>
2024-10-01 15:46:26 -0700

    Add additional Foundation repos + remove timing hack + bump version

--- Source/v2/Meadow.HCom/Connections/ConnectionBase.cs
+++ Source/v2/Meadow.HCom/Connections/ConnectionBase.cs
@@ -62,17 +62,8 @@ public abstract class ConnectionBase : IMeadowConnection, IDisposable
         ConnectionMessage?.Invoke(this, message);
     }
 
-    bool firstRun = true;
-
     protected void RaiseDebuggerMessage(byte[] data)
     {
-        if (firstRun)
-        {
-            Thread.Sleep(2000);
-            firstRun = false;
-        }
-
-        Debug.WriteLine($"*************************** RaiseDebuggerMessage: {data.Length}");
         DebuggerMessageReceived?.Invoke(this, data);
     }
 
