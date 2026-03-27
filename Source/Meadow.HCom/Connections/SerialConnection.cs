e5c268f77c669f3a9738c6bcab2a15e51765301b
Adrian Stevens <adrian.stevens@gmail.com>
2026-03-27 12:04:51 -0700

    Large file transfer + CRC fix + version bump

--- Source/Meadow.HCom/Connections/SerialConnection.cs
+++ Source/Meadow.HCom/Connections/SerialConnection.cs
@@ -282,6 +282,8 @@ public partial class SerialConnection : ConnectionBase, IDisposable
             set => _localFileName = value;
         }
         public FileStream FileStream { get; set; } = default!;
+        public uint ExpectedCrc { get; set; }
+        public uint ActualCrc { get; set; }
     }
 
     public void EnqueueRequest(IRequest command)
